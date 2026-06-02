11b4bfc47a0fc2c7ef84a75db1a1d25ad44ade23
Kotkoa <kotkoa@gmail.com>
2026-06-02 15:07:34 +0200

    fix: add remotePatterns for image optimization in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,13 @@ const nextConfig: NextConfig = {
   logging: {
     browserToTerminal: true,
   },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images2.imgbox.com' },
+      { protocol: 'https', hostname: 'imgur.com' },
+      { protocol: 'https', hostname: 'i.imgur.com' },
+    ],
+  },
 }
 
 export default nextConfig
